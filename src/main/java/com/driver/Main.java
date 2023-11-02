5c160b6683f2d3047bd4a61a0d345d64ab29a232
Soham <sohamjpathak@gmail.com>
2023-11-03 00:49:49 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] agrs){
+        RWOnly a=new RWOnly("Soham");
+
+        //System.out.print(a.name);
+        a.getter();
+        a.setter("goku");
+        a.getter();
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class RWOnly {
+
+     private String name;
+
+    public RWOnly(String name){
+        this.name=name;
+    }
+
+    public void setter(String s){
+        this.name=s;
+    }
+    public void getter(){
+        System.out.println(name);
+    }
     
 }
